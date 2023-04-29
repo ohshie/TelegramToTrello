da8cd37eb86875e50149d451c9787a1a784c8054
ohshie <114024290+ohshie@users.noreply.github.com>
2023-04-29 22:04:07 +0400

    Added migrations

---
+++ Migrations/20230429180002_initial.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TelegramToTrello.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TrelloBoardId = table.Column<string>(type: "text", nullable: false),
+                    BoardName = table.Column<string>(type: "text", nullable: false),
+                    TelegramId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CreatingTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TrelloId = table.Column<string>(type: "text", nullable: false),
+                    TaskName = table.Column<string>(type: "text", nullable: false),
+                    Tag = table.Column<string>(type: "text", nullable: false),
+                    TrelloBoardId = table.Column<string>(type: "text", nullable: false),
+                    ListId = table.Column<string>(type: "text", nullable: false),
+                    TaskId = table.Column<string>(type: "text", nullable: false),
+                    TaskDesc = table.Column<string>(type: "text", nullable: false),
+                    TaskPartId = table.Column<string>(type: "text", nullable: false),
+                    TaskPartName = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: true),
+                    NameSet = table.Column<bool>(type: "boolean", nullable: false),
+                    DescSet = table.Column<bool>(type: "boolean", nullable: false),
+                    DateSet = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreatingTasks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskNotifications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Due = table.Column<string>(type: "text", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    User = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskNotifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    TelegramId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TelegramName = table.Column<string>(type: "text", nullable: false),
+                    TrelloToken = table.Column<string>(type: "text", nullable: false),
+                    TrelloId = table.Column<string>(type: "text", nullable: false),
+                    TrelloName = table.Column<string>(type: "text", nullable: false),
+                    NotificationsEnabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.TelegramId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoardTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    TableId = table.Column<string>(type: "text", nullable: false),
+                    BoardId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardTables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BoardTables_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersOnBoards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TrelloUserId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    TrelloUserBoardId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersOnBoards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersOnBoards_Boards_TrelloUserBoardId",
+                        column: x => x.TrelloUserBoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersBoards",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    BoardId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersBoards", x => new { x.UserId, x.BoardId });
+                    table.ForeignKey(
+                        name: "FK_UsersBoards_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersBoards_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "TelegramId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoardTables_BoardId",
+                table: "BoardTables",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersBoards_BoardId",
+                table: "UsersBoards",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersOnBoards_TrelloUserBoardId",
+                table: "UsersOnBoards",
+                column: "TrelloUserBoardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoardTables");
+
+            migrationBuilder.DropTable(
+                name: "CreatingTasks");
+
+            migrationBuilder.DropTable(
+                name: "TaskNotifications");
+
+            migrationBuilder.DropTable(
+                name: "UsersBoards");
+
+            migrationBuilder.DropTable(
+                name: "UsersOnBoards");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
